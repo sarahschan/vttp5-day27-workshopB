9f15939591446556d9bc1c1983a5fa2cad2f78cd
sarahschan <sarahschan@hotmail.com>
2025-01-22 13:25:43 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/paf_day27_Workshop_B/PafDay27WorkshopBApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day27_Workshop_B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafDay27WorkshopBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafDay27WorkshopBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/paf_day27_Workshop_B/PafDay27WorkshopBApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day27_Workshop_B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafDay27WorkshopBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
